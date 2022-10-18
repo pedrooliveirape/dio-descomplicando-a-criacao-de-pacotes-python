4e9dcb175b3623f62b66f62c032d9547ddf59aae
pedrooliveirape <pedro.oliveirape@hotmail.com>
2022-10-18 10:55:09 -0300

    add docstring no arquivo funcoes_comparacao.py

--- compara_preco_litro/funcoes_comparacao.py
+++ compara_preco_litro/funcoes_comparacao.py
@@ -1,6 +1,19 @@
 from .transformartxt import arquivoexiste, lerarquivo
 
 def precoporlitro(nome):
+    """Função que por padrão retorna uma lista contendo a marca do produto e o calculo do preço por litro.
+
+    Args:
+        nome: recebe um arquivo no formato .txt contendo a marca do produto, a quantidade em litros e o preço
+        separados por vírgula e com cada produto a ser testado em uma linha.
+        Exemplo: 'coca-cola,0.350,2.99
+                  pepsi,2,6.00
+                  Guaraná,1,4.99'
+
+    Returns:
+        list: retorna uma lista contendo listas com as marcas dos produtos e os preços por litro.
+        Exemplo: [coca-cola,8.54][pepsi,3][Guaraná,4.99]
+    """
     try:
         arquivoexiste(nome)
     except:
@@ -20,6 +33,18 @@ def precoporlitro(nome):
 
 
 def melhorpreco(nome):
+    """Função que por padrão um arquivo de texto no formato .txt e retorna uma lista contendo a marca do produto e o calculo do preço por litro.
+
+    Args:
+        nome: recebe um arquivo no formato .txt contendo a marca do produto, a quantidade em litros e o preço
+        separados por vírgula e com cada produto a ser testado em uma linha.
+        Exemplo: 'coca-cola,0.350,2.99
+                  pepsi,2,6.00
+                  Guaraná,1,4.99'
+
+    Returns:
+        list: retorna uma lista contendo a marca do produto e o preço por litro do produto com melhor preço por litro.
+    """
     lista_produtos = precoporlitro(nome)
     melhor_preco = []
     for contagem, valores in enumerate(lista_produtos):
